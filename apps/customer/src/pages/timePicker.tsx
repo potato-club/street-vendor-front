e77a8d6ccef113ecd53ecde0997d4e18a00fc5b3
KoJaem <woalswhwh@naver.com>
2023-01-14 02:03:33 +0900

    style : 확인, 취소버튼 UI 구현 #7

--- apps/customer/src/pages/timePicker.tsx
+++ apps/customer/src/pages/timePicker.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useMemo, useState } from 'react';
 import styled from 'styled-components';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+
 export default function TimePickerPage() {
   const hours = useMemo(() => Array.from(new Array(12), (_, i) => i + 1), []);
 
@@ -12,7 +13,7 @@ export default function TimePickerPage() {
     []
   );
 
-  const [atnoon, setAtnoon] = useState<number>(0);
+  const [atNoon, setAtNoon] = useState<number>(0);
   const [activeHours, setActiveHours] = useState<number>(1);
   const [activeMinute, setActiveMinute] = useState<number>(0);
 
@@ -27,11 +28,12 @@ export default function TimePickerPage() {
   return (
     <Container>
       <Wrapper>
+        <Typography size="20">가게 방문 예정 시간</Typography>
         <TimeWrapper>
           <StyledSwiper
-            onSlideChange={(e) => setActiveMinute(e.realIndex)}
+            onSlideChange={(e) => setAtNoon(e.realIndex)}
             slidesPerView={3}
-            initialSlide={atnoon}
+            initialSlide={atNoon}
             centeredSlides
             direction="vertical"
           >
@@ -66,6 +68,20 @@ export default function TimePickerPage() {
             ))}
           </StyledSwiper>
         </TimeWrapper>
+        <ButtonWrapper>
+          <Button onClick={() => alert('취소')}>취소</Button>
+          <Button
+            onClick={() =>
+              alert(
+                `${
+                  atNoon === 0 ? '오전' : '오후'
+                }, ${activeHours}시, ${activeMinute}분`
+              )
+            }
+          >
+            확인
+          </Button>
+        </ButtonWrapper>
       </Wrapper>
     </Container>
   );
@@ -86,13 +102,11 @@ const Wrapper = styled.div`
   max-width: 325px;
   height: 100%;
   max-height: 470px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+  gap: 40px;
   .swiper-slide-active {
     color: black;
   }
@@ -113,4 +127,16 @@ const TimeWrapper = styled.div`
 const StyledSwiper = styled(Swiper)`
   height: 100%;
   overflow: hidden;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const Button = styled.button`
+  width: 92px;
+  height: 48px;
+  border-radius: 24px;
+  box-shadow: 0px 4px 4px ${customColor.gray}80;
 `;
