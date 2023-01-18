131b9c38b77227bc9023b0575cdc3c3f3a392b6e
JoGeumJu <ccho8702@naver.com>
2023-01-19 05:21:50 +0900

    add : 공용컴포넌트 수정 #1 - AskSelectButton의 Props 설정 변경 - AskOption의 Typography사용으로의 오류 수정(hydration error)

--- libs/core/src/components/form/AskOption.tsx
+++ libs/core/src/components/form/AskOption.tsx
@@ -1,19 +1,11 @@
 import styled from 'styled-components';
-import { customColor } from '../../constants';
-import { Typography } from '../Typography';
 
 interface OptionProps {
   value: string;
   name: string;
 }
 export const AskOption = (props: OptionProps) => {
-  return (
-    <Option value={props.value}>
-      <Typography size="12" letterSpacing="-0.5px">
-        {props.name}
-      </Typography>
-    </Option>
-  );
+  return <Option value={props.value}>{props.name}</Option>;
 };
 
 const Option = styled.option``;
