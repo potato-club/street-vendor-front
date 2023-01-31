3669a507efe812f85c7b79e53099e94711723e71
JoGeumJu <ccho8702@naver.com>
2023-02-01 01:21:54 +0900

    feat(customer): ReviewPage 추가 #18 - Review 컨테이너 숟가락별점 제외 구성완료

--- apps/customer/src/apps/review/Review.tsx
+++ apps/customer/src/apps/review/Review.tsx
@@ -1,7 +1,18 @@
-import { Typography } from '@street-vendor/core';
+import {
+  AskPhoto,
+  AskSubmit,
+  AskTextarea,
+  Typography,
+} from '@street-vendor/core';
+import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
 
 export const Review = () => {
+  const {
+    register,
+    formState: { errors },
+    watch,
+  } = useForm();
   return (
     <Container>
       <ContainerInner>
@@ -16,6 +27,27 @@ export const Review = () => {
             숟가락
           </SpoonText>
         </Spoon>
+        <ReviewText>
+          <AskTextarea
+            label="리뷰하기"
+            placeholder="리뷰 내용을 작성해주세요:)"
+            value="reviewContent"
+            register={register}
+            errors={errors}
+            watch={watch}
+          />
+        </ReviewText>
+        <ReviewPhoto>
+          <AskPhoto
+            label="첨부 사진"
+            value="reviewPhoto"
+            placeholder="사진은 최대 3장까지 등록 가능합니다."
+            errors={errors}
+          />
+        </ReviewPhoto>
+        <ReviewRegister>
+          <AskSubmit isAgreeChecked={true} onClick={() => {}} />
+        </ReviewRegister>
       </ContainerInner>
     </Container>
   );
@@ -27,11 +59,14 @@ const Container = styled.section`
   height: 100vh;
   justify-content: center;
 `;
-const ContainerInner = styled.div`
+const ContainerInner = styled.form`
   display: flex;
+  position: relative;
   flex-direction: column;
   width: 100%;
   max-width: 400px;
+  min-height: 100%;
+  height: 100%;
 `;
 const Spoon = styled.div`
   display: flex;
@@ -39,9 +74,26 @@ const Spoon = styled.div`
   width: 100%;
   margin: 24px 0;
   gap: 22px 0;
+  padding: 0 7%;
 `;
 const SpoonText = styled.div`
   display: flex;
   flex-direction: column;
   gap: 12px 0;
+`;
+const ReviewText = styled.div`
+  display: flex;
+  padding: 0 7%;
+`;
+const ReviewPhoto = styled.div`
+  display: flex;
+  padding: 0 7%;
+  margin-top: 16px;
+`;
+const ReviewRegister = styled.div`
+  display: flex;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  padding: 0 7%;
 `;
