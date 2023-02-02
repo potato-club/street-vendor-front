f3e5f4b31ec20ea5f3093298ff6fb1bd22a09d88
Pdom <pdom0327@gmail.com>
2023-02-02 21:23:31 +0900

    feat(customer): drawer

--- libs/core/src/components/navigation/drawer/NavigationDrawer.tsx
+++ libs/core/src/components/navigation/drawer/NavigationDrawer.tsx
@@ -1,10 +1,82 @@
 import styled from 'styled-components';
 import { customColor } from '../../../constants';
+import { BasicButton } from '../../button/BasicButton';
+import { Typography } from '../../Typography';
+import { NavigationDrawerItem } from './NavigationDrawerItem';
+import { ProfileImage } from './ProfileImage';
 
-export const NavigationDrawer: React.FC = () => {
+export interface NavigationDrawerProps {
+  items: { title: string; url: string }[];
+}
+
+export const NavigationDrawer: React.FC<NavigationDrawerProps> = (props) => {
   return (
     <Background>
-      <Container></Container>
+      <Container>
+        <div
+          style={{
+            display: 'flex',
+            padding: '0px 0px 32px 40px',
+          }}
+        >
+          <ProfileImage />
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              margin: '16px 0px 0px 24px',
+              gap: 12,
+              flexGrow: 1,
+            }}
+          >
+            <div style={{ display: 'flex', alignItems: 'baseline', gap: 2 }}>
+              <Typography size="16" fontWeight="bold">
+                손 안의 노점
+              </Typography>
+              <Typography size="14" fontWeight="500">
+                님
+              </Typography>
+            </div>
+            <Typography size="14" fontWeight="500">
+              맛있는 시간 되세요!
+            </Typography>
+            <BasicButton
+              backgroundColor="orange3"
+              borderRadius={100}
+              maxWidth={120}
+              height={36}
+              shadow
+            >
+              <Typography size="16" color="white">
+                프로필 변경
+              </Typography>
+            </BasicButton>
+          </div>
+        </div>
+        <div
+          style={{
+            flexGrow: 1,
+            backgroundColor: customColor.white,
+            padding: '16px 0px',
+          }}
+        >
+          {props.items.map((value) => (
+            <NavigationDrawerItem title={value.title} />
+          ))}
+        </div>
+        <BasicButton
+          style={{ margin: '24px 0px 28px 32px' }}
+          backgroundColor="orange2"
+          borderRadius={100}
+          maxWidth={120}
+          height={40}
+          shadow
+        >
+          <Typography size="16" color="white" fontWeight="bold">
+            로그아웃
+          </Typography>
+        </BasicButton>
+      </Container>
     </Background>
   );
 };
@@ -18,6 +90,9 @@ const Background = styled.div`
 `;
 
 const Container = styled.div`
+  padding-top: 64px;
+  display: flex;
+  flex-direction: column;
   height: 100%;
   margin-right: 48px;
   background-color: ${customColor.beige};
--- libs/core/src/components/navigation/drawer/ProfileImage.tsx
+++ libs/core/src/components/navigation/drawer/ProfileImage.tsx
@@ -1,12 +1,24 @@
+import { customColor } from 'libs/core/src/constants';
+import Image from 'next/image';
 import React from 'react';
 import styled from 'styled-components';
 
-export const ProfileImage = () => {
-  return <Container>ProfileImage</Container>;
+export interface ProfileImagePros {
+  src?: string;
+}
+
+export const ProfileImage: React.FC<ProfileImagePros> = (props) => {
+  return (
+    <Container>
+      <Image src={props.src ?? ''} alt={''} />
+    </Container>
+  );
 };
 
 const Container = styled.div`
   width: 108px;
   height: 108px;
   border-radius: 50%;
+  background-color: ${customColor.white};
+  box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.16) inset;
 `;
