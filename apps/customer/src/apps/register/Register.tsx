6d91c5af46f9e681cfb08ba27ad7db2737819ef4
UNNEW-main <1970005826c@gmail.com>
2023-02-09 16:31:48 +0900

    feat(customer): 회원가입 추가 #36

---
+++ apps/customer/src/apps/register/Register.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Register = () => {
+  return <Container>회원가입</Container>;
+};
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
--- apps/customer/src/pages/callback/google.tsx
+++ apps/customer/src/pages/callback/google.tsx
@@ -1,20 +1,40 @@
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
 import { authApi } from '../../apis/controller/auth.api';
+import { pathName } from '../../configs/pathName';
+import { sessionService } from '../../libs/sessionService';
 
 const GooglePage = () => {
   const { asPath } = useRouter();
-
+  const router = useRouter();
   useEffect(() => {
     const hash = asPath.split('#')[1];
     const parsedHash = new URLSearchParams(hash);
 
     const accessToken = parsedHash.get('access_token');
-    console.log(accessToken);
-    authApi.googleLogin({ accessToken });
+    handleCheckNewUser(accessToken);
   }, [asPath]);
 
-  return <div>로그인 확인 중...</div>;
+  const handleCheckNewUser = async (accessToken: string) => {
+    const response = await authApi.googleLogin({ accessToken });
+
+    if (!response.data) {
+      //로그인 실패
+      router.push(pathName.LOGIN);
+    }
+
+    if (response.data.sessionId === null) {
+      //회원가입 필요
+      sessionService.resetIdSession();
+      router.push(pathName.REGISTER);
+    } else {
+      //로그인 완료
+      sessionService.setIdSession(response.data.sessionId);
+      router.push(pathName.HOME);
+    }
+  };
+
+  return <div>회원 정보를 확인중입니다</div>;
 };
 
 export default GooglePage;
