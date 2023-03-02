3830537241f66614dae67c871bca20df349768e5
JoGeumJu <ccho8702@naver.com>
2023-03-02 22:41:17 +0900

    feat(boss) StoreRegister Recoil #40 - atomStoreReigster에 atomStoreRegisterSchedule 추가

--- apps/boss/src/recoil/atoms/atomStoreRegister.ts
+++ apps/boss/src/recoil/atoms/atomStoreRegister.ts
@@ -1,5 +1,12 @@
 import { atom } from 'recoil';
 
+export interface ScheduleType {
+  day: string;
+  isChecked: boolean;
+  open: string;
+  close: string;
+}
+
 export const atomStoreRegisterCategory = atom<string>({
   key: 'atomStoreRegisterCategory',
   default: '',
@@ -7,4 +14,16 @@ export const atomStoreRegisterCategory = atom<string>({
 export const atomStoreRegisterAccount = atom<string>({
   key: 'atomStoreRegisterAccount',
   default: '',
+});
+export const atomStoreRegisterSchedule = atom<ScheduleType[]>({
+  key: 'atomStoreRegisterSchedule',
+  default: [
+    { day: '월', isChecked: true, open: '오전 09:00', close: '오후 10:00' },
+    { day: '화', isChecked: true, open: '오전 09:00', close: '오후 10:00' },
+    { day: '수', isChecked: true, open: '오전 09:00', close: '오후 10:00' },
+    { day: '목', isChecked: true, open: '오전 09:00', close: '오후 10:00' },
+    { day: '금', isChecked: true, open: '오전 09:00', close: '오후 10:00' },
+    { day: '토', isChecked: true, open: '오전 09:00', close: '오후 10:00' },
+    { day: '일', isChecked: true, open: '오전 09:00', close: '오후 10:00' },
+  ],
 });
