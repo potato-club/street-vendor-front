ed012edb1627e45e2fc1a1267e7630e5d02ee910
Pdom <ahb0327@naver.com>
2023-04-06 19:58:08 +0900

    style(core): app bar layout

--- libs/core/src/components/layout/AppBarLayout.tsx
+++ libs/core/src/components/layout/AppBarLayout.tsx
@@ -12,11 +12,13 @@ export const AppBarLayout: React.FC<AppBarLayoutProps> = (props) => (
 
 const ChildrenWrapper = styled.div`
   padding-top: 68px;
+  width: 100%;
   height: 100%;
 `;
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100%;
   height: 100%;
 `;
