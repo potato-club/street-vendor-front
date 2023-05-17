da906d099977b6b45aeb63efa7199e584536d970
JoGeumJu <ccho8702@naver.com>
2023-05-17 16:22:50 +0900

    feat : 문의페이지 #54 - AskDetail api 연동 및 코드 수정 - pathName 수정 - useQueryGetDetailQuestion 수정

--- apps/customer/src/apps/ask/inquiry/my/AskDetail.tsx
+++ apps/customer/src/apps/ask/inquiry/my/AskDetail.tsx
@@ -1,6 +1,19 @@
+/* eslint-disable @nrwl/nx/enforce-module-boundaries */
 import { AskDetailContainer } from '@street-vendor/core';
 import { pathName } from '../../../../configs/pathName';
+import Router from 'next/router';
+import { useQueryGetDetailQuestion } from 'apps/customer/src/hooks/query/ask/useQueryGetDetailQestion';
 
 export const AskDetail = () => {
-  return <AskDetailContainer pathName={pathName.ASK.INQUIRY.FORM} />;
+  const { query } = Router;
+  const { isLoading, data } = useQueryGetDetailQuestion(
+    Number(query.questionId)
+  );
+  return (
+    <AskDetailContainer
+      pathName={pathName.ASK.INQUIRY.FORM}
+      isLoading={isLoading}
+      data={data}
+    />
+  );
 };
