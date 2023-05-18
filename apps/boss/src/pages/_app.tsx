16d98620561537b5a16a3fa998d7029456f274fe
Pdom <pdom0327@gmail.com>
2023-05-18 21:16:06 +0900

    feat(boss): 지도 관련 누락된 파일

--- apps/boss/src/pages/_app.tsx
+++ apps/boss/src/pages/_app.tsx
@@ -3,23 +3,29 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
+import { NavermapsProvider } from 'react-naver-maps';
 import './styles.css';
 
 const queryClient = new QueryClient();
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <Head>
-          <title>Welcome to boss!</title>
-        </Head>
-        <main className="app">
-          <Component {...pageProps} />
-        </main>
-        <Toast />
-      </RecoilRoot>
-    </QueryClientProvider>
+    <NavermapsProvider
+      ncpClientId={process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID}
+      submodules={['geocoder']}
+    >
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <Head>
+            <title>Welcome to boss!</title>
+          </Head>
+          <main className="app">
+            <Component {...pageProps} />
+          </main>
+          <Toast />
+        </RecoilRoot>
+      </QueryClientProvider>
+    </NavermapsProvider>
   );
 }
 
