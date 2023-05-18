4caf520080e90074f9e349fae63b17cdb2d7b155
Pdom <pdom0327@gmail.com>
2023-05-18 21:16:06 +0900

    feat(boss): 지도 관련 누락된 파일

---
+++ apps/boss/src/pages/store-register/location/index.tsx
@@ -0,0 +1,9 @@
+
+import type { NextPage } from 'next';
+import { StoreRegisterLocation } from '../../../apps/store-register/location/StoreRegisterLocation';
+
+const StoreRegisterLocationMainPage: NextPage = () => {
+  return <StoreRegisterLocation />;
+};
+
+export default StoreRegisterLocationMainPage;
