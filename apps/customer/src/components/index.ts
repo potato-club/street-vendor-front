06ff68fa4fda473dbcbe6e7d8af23dcba5ccc037
Pdom <pdom0327@gmail.com>
2023-06-15 18:31:35 +0900

    feat(customer): api

--- apps/customer/src/components/index.ts
+++ apps/customer/src/components/index.ts
@@ -1,6 +1,6 @@
 export { BottomButton } from './button/BottomButton';
+export * from './card/Store';
 export * from './card/StorePreview';
 export { Title } from './label/Title';
 export { TotalPrice } from './label/TotalPrice';
 export { Line } from './line/Line';
-
